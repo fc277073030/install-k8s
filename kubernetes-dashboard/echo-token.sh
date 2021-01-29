9c9ba3b8802ac310f6cabf224f4c8ef0574d7b5b
Your Name <you@example.com>
2021-01-30 04:56:39 +0800

    update

--- kubernetes-dashboard/echo-token.sh
+++ kubernetes-dashboard/echo-token.sh
@@ -1,1 +1,1 @@
- kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep kubernetes-dashboard-admin | awk '{print $1}')
+ kubectl -n kube-system  describe secret $(kubectl -n kube-system get secret | grep kubernetes-dashboard-admin | awk '{print $1}')
