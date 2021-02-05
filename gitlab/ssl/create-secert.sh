0d46ba66289b8547751fa44bca11fbdf782773ab
Administrator <abcd@123>
2021-02-05 16:43:26 +0800

    ssl

--- gitlab/ssl/create-secert.sh
+++ gitlab/ssl/create-secert.sh
@@ -1,4 +1,3 @@
-kubectl -n gitlab create secret  tls minio-tls  --cert=minio.fc277073030.com.pem --key=minio.fc277073030.com.key
 kubectl -n gitlab create secret  tls registry-tls  --cert=registry.fc277073030.com.pem --key=registry.fc277073030.com.key
 kubectl -n gitlab create secret  tls gitlab-tls  --cert=gitlab.fc277073030.com.pem --key=gitlab.fc277073030.com.key
 kubectl -n gitlab create secret  tls gitlab-minio-tls  --cert=gitlab-minio.fc277073030.com.pem --key=gitlab-minio.fc277073030.com.key
