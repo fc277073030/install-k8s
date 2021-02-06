7632be50725ee5b0799b254a0227e9acb1e0e98f
Administrator <abcd@123>
2021-02-06 14:01:47 +0800

    ssl

--- gitlab/ssl/create-secert.sh
+++ gitlab/ssl/create-secert.sh
@@ -1,3 +1,3 @@
-kubectl -n gitlab create secret  tls gitlab-registry-tls  --cert=registry.fc277073030.com.pem --key=registry.fc277073030.com.key
+kubectl -n gitlab create secret  tls gitlab-registry-tls  --cert=gitlab-registry.fc277073030.com.pem --key=gitlab-registry.fc277073030.com.key
 kubectl -n gitlab create secret  tls gitlab-tls  --cert=gitlab.fc277073030.com.pem --key=gitlab.fc277073030.com.key
 kubectl -n gitlab create secret  tls gitlab-minio-tls  --cert=gitlab-minio.fc277073030.com.pem --key=gitlab-minio.fc277073030.com.key
