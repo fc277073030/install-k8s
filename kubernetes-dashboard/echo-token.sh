34114b0cdc9390c92bc1930040a61866fcd994b0
fc277073030 <277073030@qq.com>
2021-02-19 11:57:15 +0800

    token

--- kubernetes-dashboard/echo-token.sh
+++ kubernetes-dashboard/echo-token.sh
@@ -1,1 +1,1 @@
- kubectl -n kube-system  describe secret $(kubectl -n kube-system get secret | grep kubernetes-dashboard-admin | awk '{print $1}')
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep eks-admin | awk '{print $1}')
